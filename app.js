34fca72a410384022102a2aa605dd74b91c967cb
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2021-03-20 22:47:58 -0300

    flujo del menu con dowhile y con promesas

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const { mostrarMenu, pause } = require('./helpers/mensajes');
 console.clear();
 
 const main = async () => {
-    console.log('prueba');
-    mostrarMenu();
-    // pause();
+    let opt = '';
+
+    do {
+        opt = await mostrarMenu();
+        console.log({ opt });
+        if (opt !== '0') await pause();
+        
+    } while ( opt !== '0' );
 }
 
 main();
