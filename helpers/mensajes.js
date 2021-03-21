9f40b914a4cf900db351e933d516cb6ada8ed5ce
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2021-03-20 21:14:56 -0300

    implemented readline

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -6,15 +6,23 @@ const mostrarMenu = () => {
     console.log('    Seleccione una opción'.green);
     console.log('=============================='.green);
 
-    console.log(`1. Crear una tarea`);
-    console.log(`2. Listar tareas`);
-    console.log(`3. Listar tareas completadas`);
-    console.log(`4. Listar tareas pendientes`);
-    console.log(`5. Completar tarea(s)`);
-    console.log(`6. Borrar tarea`);
-    console.log(`0. Salir\n`);
-    console.log(`1. `);
-    console.log(`1. `);
+    console.log(`${'1.'.green} Crear una tarea`);
+    console.log(`${'2.'.green} Listar tareas`);
+    console.log(`${'3.'.green} Listar tareas completadas`);
+    console.log(`${'4.'.green} Listar tareas pendientes`);
+    console.log(`${'5.'.green} Completar tarea(s)`);
+    console.log(`${'6.'.green} Borrar tarea`);
+    console.log(`${'0.'.green} Salir\n`);
+
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readline.question('Seleccione una opción: ', opt => {
+        console.log({ opt });
+        readline.close();
+    })
 
 };
 
